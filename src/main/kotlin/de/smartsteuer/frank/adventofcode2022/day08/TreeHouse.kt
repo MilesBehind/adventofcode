c32e3fa5e2ec4782d067ce9aa02c66c8497297d2
Frank Gerberding <frank.gerberding@smartsteuer.de>
2022-12-08 17:44:36 +0100

    cleanup

--- src/main/kotlin/de/smartsteuer/frank/adventofcode2022/day08/TreeHouse.kt
+++ src/main/kotlin/de/smartsteuer/frank/adventofcode2022/day08/TreeHouse.kt
@@ -15,12 +15,8 @@ fun part1(input: List<String>): Int {
   tailrec fun computeVisibilities(coordinates: Iterator<Coordinate>, result: Set<Coordinate>): Set<Coordinate> {
     if (!coordinates.hasNext()) return result
     val coordinate = coordinates.next()
-    val newResult =
-      if ((coordinate.left().all         { trees.height(it) < trees.height(coordinate) }) ||
-          (coordinate.right(width).all   { trees.height(it) < trees.height(coordinate) }) ||
-          (coordinate.top().all          { trees.height(it) < trees.height(coordinate) }) ||
-          (coordinate.bottom(height).all { trees.height(it) < trees.height(coordinate) })) result + coordinate
-      else result
+    val rays       = coordinate.rays(width, height)
+    val newResult  = if (rays.any { ray -> ray.all { trees.height(it) < trees.height(coordinate) } }) result + coordinate else result
     return computeVisibilities(coordinates, newResult)
   }
   val box = Coordinate.box(1..(width - 2), 1..(height - 2))
@@ -33,26 +29,22 @@ fun part2(input: List<String>): Int {
   val trees   = parseTrees(input)
   tailrec fun computeViewDistances(coordinates: Iterator<Coordinate>, result: List<Int>): List<Int> {
     if (!coordinates.hasNext()) return result
-    val coordinate = coordinates.next()
-    val distanceLeft   = coordinate.left  (      ).takeWhile { trees.height(it) < trees.height(coordinate) }.count()
-    val distanceRight  = coordinate.right (width ).takeWhile { trees.height(it) < trees.height(coordinate) }.count()
-    val distanceTop    = coordinate.top   (      ).takeWhile { trees.height(it) < trees.height(coordinate) }.count()
-    val distanceBottom = coordinate.bottom(height).takeWhile { trees.height(it) < trees.height(coordinate) }.count()
-    val viewDistance = (distanceLeft   + if (coordinate.x - distanceLeft > 0)            1 else 0) *
-                       (distanceRight  + if (coordinate.x + distanceRight < width - 1)   1 else 0) *
-                       (distanceTop    + if (coordinate.y - distanceTop > 0)             1 else 0) *
-                       (distanceBottom + if (coordinate.y + distanceBottom < height - 1) 1 else 0)
-    return computeViewDistances(coordinates, result + viewDistance)
+    val coordinate       = coordinates.next()
+    val rays             = coordinate.rays(width, height)
+    val distancesForRays = rays.map { ray -> ray.asIterable().takeUntil { trees.height(it) >= trees.height(coordinate) }.count() }
+    val scenicScore      = distancesForRays.fold(1) { acc, distance -> acc * distance }
+    return computeViewDistances(coordinates, result + scenicScore)
   }
   val box = Coordinate.box(1..(width - 2), 1..(height - 2))
   return computeViewDistances(box.iterator(), emptyList()).max()
 }
 
 data class Coordinate(val x: Int, val y: Int) {
-  fun left()              = if (x > 0)          ((x - 1) downTo     0).asSequence().map { Coordinate(it, y) } else emptySequence()
-  fun right(width: Int)   = if (x < width - 1)  ((x + 1) until  width).asSequence().map { Coordinate(it, y) } else emptySequence()
-  fun top()               = if (y > 0)          ((y - 1) downTo     0).asSequence().map { Coordinate(x, it) } else emptySequence()
-  fun bottom(height: Int) = if (y < height - 1) ((y + 1) until height).asSequence().map { Coordinate(x, it) } else emptySequence()
+  fun rays(width: Int, height: Int): Sequence<Sequence<Coordinate>> = sequenceOf(-1 to 0, 1 to 0, 0 to -1, 0 to 1).map { (deltaX, deltaY) ->
+    generateSequence(Coordinate(x + deltaX, y + deltaY)) { coordinate ->
+      Coordinate(coordinate.x + deltaX, coordinate.y + deltaY).let { result -> if (result.x in 0 until width && result.y in 0 until height) result else null }
+    }
+  }
   companion object {
     fun box(horizontalRange: IntRange, verticalRange: IntRange): Sequence<Coordinate> =
       horizontalRange.asSequence().flatMap { x ->
@@ -63,11 +55,36 @@ data class Coordinate(val x: Int, val y: Int) {
   }
 }
 
-typealias TreeGrid = Map<Coordinate, Int>
-
 fun TreeGrid.height(coordinate: Coordinate): Int = get(coordinate) ?: 0
 
 fun parseTrees(input: List<String>): TreeGrid =
   input.flatMapIndexed { y, line ->
     line.mapIndexed { x, height -> Coordinate(x, y) to height.digitToInt() }
   }.toMap()
+
+/**
+ * Take elements until the predicate evaluates to `false`.
+ * Elements will always be taken before the predicate is evaluated.
+ * - `emptyList<Int>().takeUntil    { it >= 2 }` is evaluated to `emptyList()`
+ * - `listOf(1).takeUntil           { it >= 2 }` is evaluated to `listOf(1)`
+ * - `listOf(1, 2).takeUntil        { it >= 2 }` is evaluated to `listOf(1, 2)`
+ * - `listOf(1, 2, 3).takeUntil     { it >= 2 }` is evaluated to `listOf(1, 2)`
+ * - `listOf(1, 2, 3, 4 ).takeUntil { it >= 2 }` is evaluated to `listOf(1, 2)`
+ * @param predicate this predicate will stop taking elements, if it evaluates to `true`
+ * @return elements before predicate was evaluated to `true`
+ */
+fun <T> Iterable<T>.takeUntil(predicate: (T) -> Boolean): Iterable<T> {
+  tailrec fun takeUntil(elements: Iterator<T>, result: List<T>): List<T> {
+    return when {
+      elements.hasNext() -> {
+        val element = elements.next()
+        if (predicate(element)) result + element else takeUntil(elements, result + element)
+      }
+      else               -> result
+    }
+  }
+  return takeUntil(iterator(), emptyList())
+}
+
+typealias TreeGrid = Map<Coordinate, Int>
+
--- src/test/kotlin/de/smartsteuer/frank/adventofcode2022/day08/TreeHouseKtTest.kt
+++ src/test/kotlin/de/smartsteuer/frank/adventofcode2022/day08/TreeHouseKtTest.kt
@@ -1,5 +1,6 @@
 package de.smartsteuer.frank.adventofcode2022.day08
 
+import io.kotest.matchers.collections.shouldContainExactly
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 
@@ -20,5 +21,14 @@ class TreeHouseKtTest {
   @Test
   fun `part 2 is correct`() {
     part2(input) shouldBe 8
+  }
+
+  @Test
+  fun `takeUntil takes elements until predicate is met`() {
+    emptyList<Int>().takeUntil    { it >= 2 } shouldContainExactly emptyList()
+    listOf(1).takeUntil           { it >= 2 } shouldContainExactly listOf(1)
+    listOf(1, 2).takeUntil        { it >= 2 } shouldContainExactly listOf(1, 2)
+    listOf(1, 2, 3).takeUntil     { it >= 2 } shouldContainExactly listOf(1, 2)
+    listOf(1, 2, 3, 4 ).takeUntil { it >= 2 } shouldContainExactly listOf(1, 2)
   }
 }
