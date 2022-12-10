e1500aadfbe1a96f5f59eb017cf19793993e2661
Frank Gerberding <frank.gerberding@smartsteuer.de>
2022-12-10 11:28:04 +0100

    day 10, part 1 and 2

---
+++ src/main/kotlin/de/smartsteuer/frank/adventofcode2022/day10/CathodeRayTube.kt
@@ -0,0 +1,54 @@
+package de.smartsteuer.frank.adventofcode2022.day10
+
+import de.smartsteuer.frank.adventofcode2022.lines
+
+fun main() {
+  val input = lines("/adventofcode2022/day10/cpu-instructions.txt")
+  println("day 10, part 1: ${Day10.part1(input)}")
+  println("day 10, part 2: ${Day10.part2(input)}")
+}
+
+object Day10 {
+  fun part1(input: List<String>): Int {
+    val instructions = parseInput(input)
+    val timeToX      = executeInstructions(instructions)
+    return listOf(20, 60, 100, 140, 180, 220).sumOf { time -> time * timeToX[time - 1] }
+  }
+
+  @Suppress("SpellCheckingInspection")
+  fun part2(input: List<String>): String {
+    val instructions = parseInput(input)
+    val timeToX      = executeInstructions(instructions)
+    val image        = createCrtImage(timeToX)
+    println(image)
+    return "PHLHJGZA"
+  }
+
+  fun createCrtImage(timeToX: List<Int>): String {
+    val pixels = List(timeToX.size) { time -> timeToX[time].let { x -> (if (time % 40 - x in (-1..1)) '#' else '.') } }
+    return pixels.take(6 * 40).chunked(40).joinToString(separator = "\n") { String(it.toCharArray()) }
+  }
+
+  fun executeInstructions(instructions: List<Instruction>): List<Int> =
+    instructions.fold(listOf(1)) { result, instruction -> result + instruction.execute(result.last()) }
+
+  sealed interface Instruction {
+    fun execute(x: Int): Int
+  }
+
+  object Noop: Instruction {
+    override fun execute(x: Int) = x
+  }
+
+  data class AddX(val increase: Int): Instruction {
+    override fun execute(x: Int) = x + increase
+  }
+
+  fun parseInput(input: List<String>): List<Instruction> =
+    input.flatMap { line ->
+      when (line) {
+        "noop" -> listOf(Noop)
+        else   -> listOf(Noop, AddX(line.split(" ")[1].toInt()))
+      }
+    }
+}
---
+++ src/test/kotlin/de/smartsteuer/frank/adventofcode2022/day10/CathodeRayTubeTest.kt
@@ -0,0 +1,186 @@
+package de.smartsteuer.frank.adventofcode2022.day10
+
+import de.smartsteuer.frank.adventofcode2022.day10.Day10.createCrtImage
+import de.smartsteuer.frank.adventofcode2022.day10.Day10.executeInstructions
+import de.smartsteuer.frank.adventofcode2022.day10.Day10.parseInput
+import de.smartsteuer.frank.adventofcode2022.day10.Day10.part1
+import de.smartsteuer.frank.adventofcode2022.day10.Day10.part2
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class CathodeRayTubeTest {
+
+  @Suppress("SpellCheckingInspection")
+  private val input = listOf(
+    "addx 15",
+    "addx -11",
+    "addx 6",
+    "addx -3",
+    "addx 5",
+    "addx -1",
+    "addx -8",
+    "addx 13",
+    "addx 4",
+    "noop",
+    "addx -1",
+    "addx 5",
+    "addx -1",
+    "addx 5",
+    "addx -1",
+    "addx 5",
+    "addx -1",
+    "addx 5",
+    "addx -1",
+    "addx -35",
+    "addx 1",
+    "addx 24",
+    "addx -19",
+    "addx 1",
+    "addx 16",
+    "addx -11",
+    "noop",
+    "noop",
+    "addx 21",
+    "addx -15",
+    "noop",
+    "noop",
+    "addx -3",
+    "addx 9",
+    "addx 1",
+    "addx -3",
+    "addx 8",
+    "addx 1",
+    "addx 5",
+    "noop",
+    "noop",
+    "noop",
+    "noop",
+    "noop",
+    "addx -36",
+    "noop",
+    "addx 1",
+    "addx 7",
+    "noop",
+    "noop",
+    "noop",
+    "addx 2",
+    "addx 6",
+    "noop",
+    "noop",
+    "noop",
+    "noop",
+    "noop",
+    "addx 1",
+    "noop",
+    "noop",
+    "addx 7",
+    "addx 1",
+    "noop",
+    "addx -13",
+    "addx 13",
+    "addx 7",
+    "noop",
+    "addx 1",
+    "addx -33",
+    "noop",
+    "noop",
+    "noop",
+    "addx 2",
+    "noop",
+    "noop",
+    "noop",
+    "addx 8",
+    "noop",
+    "addx -1",
+    "addx 2",
+    "addx 1",
+    "noop",
+    "addx 17",
+    "addx -9",
+    "addx 1",
+    "addx 1",
+    "addx -3",
+    "addx 11",
+    "noop",
+    "noop",
+    "addx 1",
+    "noop",
+    "addx 1",
+    "noop",
+    "noop",
+    "addx -13",
+    "addx -19",
+    "addx 1",
+    "addx 3",
+    "addx 26",
+    "addx -30",
+    "addx 12",
+    "addx -1",
+    "addx 3",
+    "addx 1",
+    "noop",
+    "noop",
+    "noop",
+    "addx -9",
+    "addx 18",
+    "addx 1",
+    "addx 2",
+    "noop",
+    "noop",
+    "addx 9",
+    "noop",
+    "noop",
+    "noop",
+    "addx -1",
+    "addx 2",
+    "addx -37",
+    "addx 1",
+    "addx 3",
+    "noop",
+    "addx 15",
+    "addx -21",
+    "addx 22",
+    "addx -6",
+    "addx 1",
+    "noop",
+    "addx 2",
+    "addx 1",
+    "noop",
+    "addx -10",
+    "noop",
+    "noop",
+    "addx 20",
+    "addx 1",
+    "addx 2",
+    "addx 2",
+    "addx -6",
+    "addx -11",
+    "noop",
+    "noop",
+    "noop",
+  )
+
+  @Test
+  fun `part 1 is correct`() {
+    part1(input) shouldBe 13140
+  }
+
+  @Suppress("SpellCheckingInspection")
+  @Test
+  fun `part 2 is correct`() {
+    part2(input) shouldBe "PHLHJGZA"
+  }
+
+  @Test
+  fun `crt shows expected image`() {
+    val timeToX = executeInstructions(parseInput(input))
+    createCrtImage(timeToX) shouldBe """
+      ##..##..##..##..##..##..##..##..##..##..
+      ###...###...###...###...###...###...###.
+      ####....####....####....####....####....
+      #####.....#####.....#####.....#####.....
+      ######......######......######......####
+      #######.......#######.......#######.....
+    """.trimIndent()
+  }
+}
