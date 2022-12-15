8d91a00f1de2b5f24df402f39ed424b6df2ea573
Frank Gerberding <frank.gerberding@smartsteuer.de>
2022-12-15 17:13:10 +0100

    polishing

--- src/main/kotlin/de/smartsteuer/frank/adventofcode2022/day14/RegolithReservoir.kt
+++ src/main/kotlin/de/smartsteuer/frank/adventofcode2022/day14/RegolithReservoir.kt
@@ -1,8 +1,7 @@
 package de.smartsteuer.frank.adventofcode2022.day14
 
-import de.smartsteuer.frank.adventofcode2022.day14.Day14.CellType
-import de.smartsteuer.frank.adventofcode2022.day14.Day14.CellType.*
-import de.smartsteuer.frank.adventofcode2022.day14.Day14.Coordinate
+import de.smartsteuer.frank.adventofcode2022.day14.Day14.Cave
+import de.smartsteuer.frank.adventofcode2022.day14.Day14.Cave.Coordinate
 import de.smartsteuer.frank.adventofcode2022.lines
 import kotlin.math.abs
 
@@ -12,65 +11,67 @@ fun main() {
   println("day 14, part 2: ${Day14.part2(input)}")
 }
 
-typealias Cells       = Map<Coordinate, CellType>
-typealias CellChecker = (Cells, Coordinate) -> Boolean
+typealias CellChecker = (Cave, Coordinate) -> Boolean
 
 object Day14 {
   private const val SOURCE_X = 500
   private const val SOURCE_Y =   0
 
   fun part1(input: List<String>): Int {
-    val isFinished  = { cells: Cells, grain: Coordinate -> cells.keys.maxBy { it.y }.let { abyss -> grain.y > abyss.y } }
-    val isCollision = { cells: Cells, grain: Coordinate -> grain in cells.keys }
+    val isFinished  = { cave: Cave, grain: Coordinate -> grain.y > cave.maxRockY }
+    val isCollision = { cave: Cave, grain: Coordinate -> grain in cave.rockCells || grain in cave.sandCells }
     return parseAndSimulate(input, isFinished, isCollision)
   }
 
   fun part2(input: List<String>): Int {
-    val isFinished  = { _: Cells, grain: Coordinate -> grain.x == SOURCE_X && grain.y == SOURCE_Y }
-    val isCollision = { cells: Cells, grain: Coordinate ->
-      grain in cells.keys || grain.y >= cells.filter { (_, type) -> type == ROCK }.keys.maxBy { it.y }.y + 2 }
+    val isFinished  = { _: Cave, grain: Coordinate -> grain.x == SOURCE_X && grain.y == SOURCE_Y }
+    val isCollision = { cave: Cave, grain: Coordinate -> grain in cave.rockCells || grain in cave.sandCells || grain.y >= cave.maxRockY + 2 }
     return parseAndSimulate(input, isFinished, isCollision) + 1
   }
 
   private fun parseAndSimulate(input: List<String>, isFinished:  CellChecker, isCollision: CellChecker): Int {
-    val cave         = Cave.parse(input).also { println(it) }
-    val caveWithSand = cave.simulate(Coordinate(SOURCE_X, SOURCE_Y), isFinished, isCollision).also { println(it) }
+    val cave         = Cave.parse(input)//.also { println(it) }
+    val caveWithSand = cave.simulate(Coordinate(SOURCE_X, SOURCE_Y), isFinished, isCollision)//.also { println(it) }
     return caveWithSand.grainCount()
   }
 
-  data class Coordinate(val x: Int, val y: Int) {
-    fun sandTargets(): List<Coordinate> = listOf(Coordinate(x, y + 1), Coordinate(x - 1, y + 1), Coordinate(x + 1, y + 1))
-  }
 
-  enum class CellType { ROCK, SAND }
+  data class Cave constructor(val rockCells: Set<Coordinate>, val sandCells: Set<Coordinate>, val maxRockY: Int) {
 
-  data class Cave(val cells: Map<Coordinate, CellType>) {
-    fun simulate(grainSource: Coordinate, isFinished: CellChecker, isCollision: CellChecker): Cave {
-      tailrec fun simulate(cells: Map<Coordinate, CellType>, grain: Coordinate): Cave {
-        val newGrain = grain.sandTargets().dropWhile { isCollision(cells, it) }.firstOrNull() ?: grain
-        return when {
-          isFinished(cells, newGrain) -> Cave(cells)
-          newGrain == grain           -> { simulate(cells + (grain to SAND), grainSource) }
-          else                        -> simulate(cells, newGrain)
-        }
-      }
-      return simulate(cells, grainSource)
+    data class Coordinate(val x: Int, val y: Int) {
+      fun sandTargets(): List<Coordinate> = listOf(Coordinate(x, y + 1), Coordinate(x - 1, y + 1), Coordinate(x + 1, y + 1))
     }
 
-    fun grainCount() = cells.values.count { it == SAND }
+    constructor(rockCells: Set<Coordinate>, sandCells: Set<Coordinate> = emptySet()): this(rockCells, sandCells, rockCells.maxBy { it.y }.y)
+
+    fun simulate(grainSource: Coordinate, isFinished: CellChecker, isCollision: CellChecker): Cave {
+      tailrec fun simulate(sandCells: Set<Coordinate>, grain: Coordinate): Cave {
+        val cave = Cave(rockCells, sandCells, maxRockY)//.also { println(it) }
+        val newGrain = grain.sandTargets().dropWhile { isCollision(cave, it) }.firstOrNull() ?: grain
+        return when {
+          isFinished(this, newGrain) -> Cave(rockCells, sandCells, maxRockY)
+          newGrain == grain          -> simulate(sandCells + grain, grainSource)
+          else                       -> simulate(sandCells, newGrain)
+        }
+      }
+      return simulate(sandCells, grainSource)
+    }
+
+    fun grainCount() = sandCells.size
 
     override fun toString(): String {
-      val xRange = (cells.keys.minBy { it.x }.x)..(cells.keys.maxBy { it.x }.x)
-      val yRange = (cells.keys.minBy { it.y }.y)..(cells.keys.maxBy { it.y }.y)
+      val allCells = rockCells + sandCells
+      val xRange = (allCells.minBy { it.x }.x)..(allCells.maxBy { it.x }.x)
+      val yRange = (allCells.minBy { it.y }.y)..(allCells.maxBy { it.y }.y)
       return buildString {
         yRange.map { y ->
           xRange.map { x ->
             val coordinate = Coordinate(x, y)
-            append(when {
-                     cells[coordinate] == ROCK -> '▓'
-                     cells[coordinate] == SAND -> 'o'
-                     else                      -> '░'
-                   })
+            append(when (coordinate) {
+              in rockCells -> '▓'
+              in sandCells -> 'o'
+              else         -> '░'
+            })
           }
           appendLine()
         }
@@ -95,7 +96,7 @@ object Day14 {
             }
           }.flatten()
         }
-        return Cave(rocks.associateWith { ROCK })
+        return Cave(rocks.toSet())
       }
     }
   }
