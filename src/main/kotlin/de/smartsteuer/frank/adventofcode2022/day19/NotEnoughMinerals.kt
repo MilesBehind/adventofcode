e102e21439d713ae38ca124ef362ab682cdee905
Frank Gerberding <frank.gerberding@smartsteuer.de>
2022-12-19 19:14:59 +0100

    Day 19, part 1, part 2

--- src/main/kotlin/de/smartsteuer/frank/adventofcode2022/day19/NotEnoughMinerals.kt
+++ src/main/kotlin/de/smartsteuer/frank/adventofcode2022/day19/NotEnoughMinerals.kt
@@ -1,15 +1,15 @@
 package de.smartsteuer.frank.adventofcode2022.day19
 
-import de.smartsteuer.frank.adventofcode2022.day19.Day19.Resource.*
 import de.smartsteuer.frank.adventofcode2022.day19.Day19.Robot.*
 import de.smartsteuer.frank.adventofcode2022.day19.Day19.part1
 import de.smartsteuer.frank.adventofcode2022.day19.Day19.part2
 import de.smartsteuer.frank.adventofcode2022.extractNumbers
 import de.smartsteuer.frank.adventofcode2022.linesSequence
-import de.smartsteuer.frank.adventofcode2022.replaced
+import java.text.DecimalFormat
 import kotlin.system.measureTimeMillis
 
-private const val MINUTES = 24
+private const val MINUTES_PART_1 = 24
+private const val MINUTES_PART_2 = 32
 
 fun main() {
   measureTimeMillis {
@@ -20,83 +20,130 @@ fun main() {
   }.also { println("took $it ms") }
 }
 
+@Suppress("SimplifiableCallChain")
 object Day19 {
   fun part1(input: Sequence<String>): Int {
     val blueprints = parseBlueprints(input)
-    return blueprints.sumOf { blueprint -> blueprint.computeQualityLevel(MINUTES) }
+    return blueprints.sumOf { blueprint -> blueprint.computeQualityLevel(MINUTES_PART_1) }
   }
 
-  fun part2(input: Sequence<String>): Int = 0
-
-  enum class Resource { NOTHING, ORE, CLAY, OBSIDIAN, GEODE }
+  fun part2(input: Sequence<String>): Int {
+    val blueprints = parseBlueprints(input.take(3))
+    val geodes = blueprints.map { blueprint -> blueprint.computeMaximumGeodes(MINUTES_PART_2) }
+    return geodes.fold(1) { product, geode -> product * geode }
+  }
 
   sealed interface Robot {
     val costsOre:      Int get() = 0
     val costsClay:     Int get() = 0
     val costsObsidian: Int get() = 0
-    val produces:      Resource
 
-    val neededResources get() = lazy { listOf(0, costsOre, costsClay, costsObsidian, 0) }
+    fun canBeProduced(inventory: Inventory): Boolean
+    fun produce(inventory: Inventory): Inventory
+    fun collect(inventory: Inventory): Inventory
 
-    fun canBeProduced(inventory: Inventory): Boolean =
-      neededResources.value.mapIndexed { resource, count -> inventory.resources[resource] >= count }.all { it }
+    object NoRobot: Robot {
+      override fun canBeProduced(inventory: Inventory) = true
+      override fun produce(inventory: Inventory) = inventory
+      override fun collect(inventory: Inventory) = inventory
+      override fun toString() = "NoRobot"
+    }
 
-    fun produce(inventory: Inventory): Inventory =
-      inventory.copy(resources = inventory.resources.mapIndexed { resource, count -> count - neededResources.value[resource] },
-                     robots    = inventory.robots.replaced(produces.ordinal, inventory.robots[produces.ordinal] + 1))
+    data class OreRobot(override val costsOre: Int): Robot {
+      override fun canBeProduced(inventory: Inventory) = inventory.ore >= costsOre
+      override fun produce(inventory: Inventory) = inventory.copy(ore = inventory.ore - costsOre, oreRobots = inventory.oreRobots + 1)
+      override fun collect(inventory: Inventory) = inventory.copy(ore = inventory.ore + 1)
+    }
 
-    object NoRobot: Robot { override val produces = NOTHING; override fun toString() = "NoRobot" }
-    data class OreRobot     (override val costsOre: Int): Robot { override val produces = ORE }
-    data class ClayRobot    (override val costsOre: Int): Robot { override val produces = CLAY }
-    data class ObsidianRobot(override val costsOre: Int, override val costsClay:     Int): Robot { override val produces = OBSIDIAN }
-    data class GeodeRobot   (override val costsOre: Int, override val costsObsidian: Int): Robot { override val produces = GEODE }
+    data class ClayRobot(override val costsOre: Int): Robot  {
+      override fun canBeProduced(inventory: Inventory) = inventory.ore >= costsOre
+      override fun produce(inventory: Inventory) = inventory.copy(ore = inventory.ore - costsOre, clayRobots = inventory.clayRobots + 1)
+      override fun collect(inventory: Inventory) = inventory.copy(clay = inventory.clay + 1)
+    }
+
+    data class ObsidianRobot(override val costsOre: Int, override val costsClay: Int): Robot  {
+      override fun canBeProduced(inventory: Inventory) = inventory.ore >= costsOre && inventory.clay >= costsClay
+      override fun produce(inventory: Inventory) = inventory.copy(ore = inventory.ore - costsOre, clay = inventory.clay - costsClay, obsidianRobots = inventory.obsidianRobots + 1)
+      override fun collect(inventory: Inventory) = inventory.copy(obsidian = inventory.obsidian + 1)
+    }
+
+    data class GeodeRobot(override val costsOre: Int, override val costsObsidian: Int): Robot  {
+      override fun canBeProduced(inventory: Inventory) = inventory.ore >= costsOre && inventory.obsidian >= costsObsidian
+      override fun produce(inventory: Inventory) = inventory.copy(ore = inventory.ore - costsOre, obsidian = inventory.obsidian - costsObsidian, geodeRobots = inventory.geodeRobots + 1)
+      override fun collect(inventory: Inventory) = inventory.copy(geode = inventory.geode + 1)
+    }
   }
 
-  data class Inventory(val resources: List<Int>, val robots: List<Int>)  // uses resource.ordinal as index
+  data class Inventory(val ore:       Int, val clay:       Int, val obsidian:       Int, val geode:       Int,
+                       val oreRobots: Int, val clayRobots: Int, val obsidianRobots: Int, val geodeRobots: Int): Comparable<Inventory> {
 
+    fun resourceCount(robot: Robot) = when (robot) {
+      is NoRobot       -> 0
+      is OreRobot      -> ore
+      is ClayRobot     -> clay
+      is ObsidianRobot -> obsidian
+      is GeodeRobot    -> geode
+    }
 
+    fun collectResources(minutes: Int = 1) = copy(
+      ore      = ore      + minutes * oreRobots,
+      clay     = clay     + minutes * clayRobots,
+      obsidian = obsidian + minutes * obsidianRobots,
+      geode    = geode    + minutes * geodeRobots
+    )
+
+    override fun compareTo(other: Inventory): Int {
+      val geodeResult = geode.compareTo(other.geode)
+      if (geodeResult != 0) return geodeResult
+      val obsidianResult = obsidian.compareTo(other.obsidian)
+      if (obsidianResult != 0) return obsidianResult
+      val clayResult = clay.compareTo(other.clay)
+      if (clayResult != 0) return clayResult
+      return ore.compareTo(other.ore)
+    }
+  }
 
   data class Blueprint(val number: Int, val oreRobot: OreRobot, val clayRobot: ClayRobot, val obsidianRobot: ObsidianRobot, val geodeRobot: GeodeRobot) {
 
     private val robots = listOf(NoRobot, oreRobot, clayRobot, obsidianRobot, geodeRobot)
 
     private val maximumNeededResources = listOf(10_000,  // nothing is always on our shopping list
-                                                robots.maxOf { it.costsOre },
-                                                robots.maxOf { it.costsClay },
+                                                robots.maxOf { it.costsOre } * 2,
+                                                robots.maxOf { it.costsClay } * 2,
                                                 robots.maxOf { it.costsObsidian } * 2,
                                                 10_000)  // geode is always on our shopping list
 
-    private val startInventory = Inventory(listOf(0, 0, 0, 0, 0),  // NOTHING, ORE, CLAY, OBSIDIAN, GEODE
-                                           listOf(0, 1, 0, 0, 0))  // NOTHING, ORE, CLAY, OBSIDIAN, GEODE
+    private val startInventory = Inventory(ore       = 0, clay       = 0, obsidian       = 0, geode       = 0,
+                                           oreRobots = 1, clayRobots = 0, obsidianRobots = 0, geodeRobots = 0)
 
-    private fun computeMaximumGeodesAfter(minutes: Int): Int {
-      val inventory = computeInventoryAfter(minutes, startInventory)
-      println("inventory after $minutes minutes: $inventory")
-      return inventory.resources[GEODE.ordinal]
+    fun computeMaximumGeodes(minutes: Int): Int {
+      val inventory: Inventory = computeInventory(minutes, startInventory)
+      println("after $minutes minutes and ${DecimalFormat().format(calls)} calls: inventory = $inventory")
+      return inventory.geode
     }
 
-    private fun computeInventoryAfter(minutes: Int, inventory: Inventory): Inventory {
-      //println("computeInventoryAfter(${24 - minutes + 1}, $inventory)")
-      if (minutes == 0) {
-        println("time is up, inventory: $inventory")
-        return inventory
+    private var calls = 0
+
+    private data class State(val minutesLeft: Int, val inventory: Inventory)
+
+    private val stateCache = mutableMapOf<State, Inventory>()
+
+    private fun computeInventory(minutesLeft: Int, inventory: Inventory): Inventory {
+      return stateCache.getOrPut(State(minutesLeft, inventory)) {
+        calls++
+        //if (calls++ % 1_000_000 == 0) println("after ${calls / 1_000_000} million calls")
+        if (minutesLeft == 0) return@getOrPut inventory
+        val inventoryAfterCollectingResources = inventory.collectResources(1)
+        if (geodeRobot.canBeProduced(inventory)) return@getOrPut computeInventory(minutesLeft - 1, geodeRobot.produce(inventoryAfterCollectingResources))
+        return@getOrPut robots
+          .filter { robot -> robot.canBeProduced(inventory) }
+          .filter { robot -> inventoryAfterCollectingResources.resourceCount(robot) < maximumNeededResources[robots.indexOf(robot)] }
+          .map { robot -> computeInventory(minutesLeft - 1, robot.produce(inventoryAfterCollectingResources)) }
+          .max()
       }
-      val inventoryAfterCollectingResources = inventory.copy(resources = inventory.resources.mapIndexed { resource, count ->
-        count + inventory.robots[resource]
-      })
-      val productionCandidates = robots
-        .filter { robot -> robot.canBeProduced(inventory) }
-        .filterIndexed { resource, _ -> inventoryAfterCollectingResources.resources[resource] < maximumNeededResources[resource] }
-      //println("producibleRobots: $producibleRobots")
-      return productionCandidates
-        .map { robot ->
-          //println("produce $robot...")
-          computeInventoryAfter(minutes - 1, robot.produce(inventoryAfterCollectingResources))
-        }
-        .maxBy { finalInventory -> finalInventory.resources[GEODE.ordinal] }
     }
 
-    fun computeQualityLevel(minutes: Int) = computeMaximumGeodesAfter(minutes) * number
+    fun computeQualityLevel(minutes: Int) = computeMaximumGeodes(minutes) * number
   }
 
   fun parseBlueprints(input: Sequence<String>): List<Blueprint> =
--- src/test/kotlin/de/smartsteuer/frank/adventofcode2022/day19/NotEnoughMineralsTest.kt
+++ src/test/kotlin/de/smartsteuer/frank/adventofcode2022/day19/NotEnoughMineralsTest.kt
@@ -16,19 +16,39 @@ class NotEnoughMineralsTest {
 
   @Test
   fun `part 1 is correct`() {
-    part1(input) shouldBe 9 + 24
+    part1(input) shouldBe 9 * 1 + 12 * 2  // real data: 1659
   }
 
   @Test
   fun `part 2 is correct`() {
-    part2(input) shouldBe 58
+    part2(input) shouldBe 56 * 62  // real data: 6804
   }
 
   @Test
   fun `blueprints can be parsed`() {
     parseBlueprints(input) shouldBe listOf(
-      Blueprint(1, OreRobot(costsOre = 4), ClayRobot(costsOre = 2), ObsidianRobot(costsOre = 3, costsClay = 14), GeodeRobot(costsOre = 2, costsObsidian = 7)),
+      Blueprint(1, OreRobot(costsOre = 4), ClayRobot(costsOre = 2), ObsidianRobot(costsOre = 3, costsClay = 14), GeodeRobot(costsOre = 2, costsObsidian =  7)),
       Blueprint(2, OreRobot(costsOre = 2), ClayRobot(costsOre = 3), ObsidianRobot(costsOre = 3, costsClay =  8), GeodeRobot(costsOre = 3, costsObsidian = 12))
     )
   }
 }
+
+/*
+
+plan 1
+           minute
+           1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
+    ore    1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
+  1 clay   0  0  0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21
+  2 clay   0  0  0  1  2  4  6  8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 40
+  3 clay   0  0  0  1  2  4  6  9 12 15 18 21 24 27 30 33 36 39 42 45 48 51 54 57
+
+plan 2
+           minute
+           1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
+    ore    1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
+  1 clay   0  0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22
+  2 clay   0  0  1  2  4  6  8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 40 42
+  3 clay   0  0  1  2  4  6  9 12 15 18 21 24 27 30 33 36 39 42 45 48 51 54 57 60
+
+*/
