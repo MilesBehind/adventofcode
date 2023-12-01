6e079e720d9035b05e23ce50264b40f24d55df1b
Frank Gerberding <frank.gerberding@smartsteuer.de>
2023-12-01 14:56:51 +0100

    better version of day 01

--- src/main/kotlin/de/smartsteuer/frank/adventofcode2023/day01/Trebuchet.kt
+++ src/main/kotlin/de/smartsteuer/frank/adventofcode2023/day01/Trebuchet.kt
@@ -4,23 +4,15 @@ import de.smartsteuer.frank.adventofcode2023.lines
 
 fun main() {
   val calibrationInput: List<String> = lines("/adventofcode2023/day01/calibration.txt")
-  part1(calibrationInput)
-  part2(calibrationInput)
+  println("part 1: ${part1(calibrationInput)}")
+  println("part 2: ${part2(calibrationInput)}")
 }
 
-internal fun part1(calibrationInput: List<String>): Int {
-  val calibrationValues = calibrationInput.map { calibrationValue(it, toDigits) }
-  val sum = calibrationValues.sum()
-  println("part 1: calibration values = $calibrationValues,\nsum = $sum")
-  return sum
-}
+internal fun part1(calibrationInput: List<String>): Int =
+  calibrationInput.map { calibrationValue(it, toDigits) }.also { println("values: $it") }.sum()
 
-internal fun part2(calibrationInput: List<String>): Int {
-  val calibrationValues = calibrationInput.map { calibrationValue(it, toDigitsIncludingText) }
-  val sum = calibrationValues.sum()
-  println("part 2: calibration values = $calibrationValues,\nsum = $sum")
-  return sum
-}
+internal fun part2(calibrationInput: List<String>): Int =
+  calibrationInput.map { calibrationValue(it, toDigitsIncludingText) }.also { println("values: $it") }.sum()
 
 private fun calibrationValue(input: String, converter: (String) -> List<Int>): Int =
   converter(input).let { digits -> digits.first() * 10 + digits.last() }
