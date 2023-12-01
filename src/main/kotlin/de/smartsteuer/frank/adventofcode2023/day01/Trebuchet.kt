f1d610f7a96bc8fb94db283519ef9574b29fdae8
Frank Gerberding <frank.gerberding@smartsteuer.de>
2023-12-01 16:06:12 +0100

    better version of day 01

--- src/main/kotlin/de/smartsteuer/frank/adventofcode2023/day01/Trebuchet.kt
+++ src/main/kotlin/de/smartsteuer/frank/adventofcode2023/day01/Trebuchet.kt
@@ -9,17 +9,15 @@ fun main() {
 }
 
 internal fun part1(calibrationInput: List<String>): Int =
-  calibrationInput.map { calibrationValue(it, toDigits) }.also { println("values: $it") }.sum()
+  calibrationInput.map { calibrationValue(it, digits) }.also { println("values: $it") }.sum()
 
 internal fun part2(calibrationInput: List<String>): Int =
-  calibrationInput.map { calibrationValue(it, toDigitsIncludingText) }.also { println("values: $it") }.sum()
+  calibrationInput.map { calibrationValue(it, digits + words) }.also { println("values: $it") }.sum()
 
-private fun calibrationValue(input: String, converter: (String) -> List<Int>): Int =
-  converter(input).let { digits -> digits.first() * 10 + digits.last() }
+private fun calibrationValue(input: String, numbers: Map<String, Int>): Int =
+  toDigitsIncludingText(input, numbers).let { digits -> digits.first() * 10 + digits.last() }
 
-private val toDigits = { input: String -> input.filter { it.isDigit() }.map { it.digitToInt() } }
-
-private val toDigitsIncludingText = { input: String ->
+private fun toDigitsIncludingText(input: String, numbers: Map<String, Int>) =
   input.indices
     .map { input.substring(it) }
     .mapNotNull { subInput ->
@@ -28,6 +26,6 @@ private val toDigitsIncludingText = { input: String ->
         else                       -> numbers.entries.find { (word, _) -> subInput.startsWith(word) }?.value
       }
     }
-}
 
-private val numbers = mapOf("one" to 1, "two" to 2, "three" to 3, "four" to 4, "five" to 5, "six" to 6, "seven" to 7, "eight" to 8, "nine" to 9)
+private val digits = mapOf("1"   to 1, "2"   to 2, "3"     to 3, "4"    to 4, "5"    to 5, "6"   to 6, "7"     to 7, "8"     to 8, "9"    to 9)
+private val words  = mapOf("one" to 1, "two" to 2, "three" to 3, "four" to 4, "five" to 5, "six" to 6, "seven" to 7, "eight" to 8, "nine" to 9)
