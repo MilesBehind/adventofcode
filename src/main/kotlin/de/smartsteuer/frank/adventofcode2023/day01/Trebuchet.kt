0e25ba00a73f9821a18759fb1f4e8332eb477404
Frank Gerberding <frank.gerberding@smartsteuer.de>
2023-12-01 14:52:44 +0100

    better version of day 01

--- src/main/kotlin/de/smartsteuer/frank/adventofcode2023/day01/Trebuchet.kt
+++ src/main/kotlin/de/smartsteuer/frank/adventofcode2023/day01/Trebuchet.kt
@@ -3,35 +3,31 @@ package de.smartsteuer.frank.adventofcode2023.day01
 import de.smartsteuer.frank.adventofcode2023.lines
 
 fun main() {
-  val calibration: List<String> = lines("/adventofcode2023/day01/calibration.txt")
-  part1(calibration)
-  part2(calibration)
+  val calibrationInput: List<String> = lines("/adventofcode2023/day01/calibration.txt")
+  part1(calibrationInput)
+  part2(calibrationInput)
 }
 
-internal fun part1(calibration: List<String>): Int {
-  val calibrationValues = calibration.map { calibrationValue(it) }
+internal fun part1(calibrationInput: List<String>): Int {
+  val calibrationValues = calibrationInput.map { calibrationValue(it, toDigits) }
   val sum = calibrationValues.sum()
   println("part 1: calibration values = $calibrationValues,\nsum = $sum")
   return sum
 }
 
-internal fun part2(calibration: List<String>): Int {
-  val calibrationValues = calibration.map { calibrationValueIncludingText(it) }
+internal fun part2(calibrationInput: List<String>): Int {
+  val calibrationValues = calibrationInput.map { calibrationValue(it, toDigitsIncludingText) }
   val sum = calibrationValues.sum()
   println("part 2: calibration values = $calibrationValues,\nsum = $sum")
   return sum
 }
 
-private fun calibrationValue(input: String): Int =
-  toDigits(input).let { digits -> digits.first() * 10 + digits.last() }
+private fun calibrationValue(input: String, converter: (String) -> List<Int>): Int =
+  converter(input).let { digits -> digits.first() * 10 + digits.last() }
 
-private fun calibrationValueIncludingText(input: String): Int =
-  toDigitsIncludingText(input).let { digits -> digits.first() * 10 + digits.last() }
+private val toDigits = { input: String -> input.filter { it.isDigit() }.map { it.digitToInt() } }
 
-private fun toDigits(input: String): List<Int> =
-  input.filter { it.isDigit() }.map { it.digitToInt() }
-
-private fun toDigitsIncludingText(input: String) =
+private val toDigitsIncludingText = { input: String ->
   input.indices
     .map { input.substring(it) }
     .mapNotNull { subInput ->
@@ -40,5 +36,6 @@ private fun toDigitsIncludingText(input: String) =
         else                       -> numbers.entries.find { (word, _) -> subInput.startsWith(word) }?.value
       }
     }
+}
 
 private val numbers = mapOf("one" to 1, "two" to 2, "three" to 3, "four" to 4, "five" to 5, "six" to 6, "seven" to 7, "eight" to 8, "nine" to 9)
