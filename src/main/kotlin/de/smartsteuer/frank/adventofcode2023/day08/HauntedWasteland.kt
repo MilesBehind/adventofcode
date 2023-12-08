9bb3fec2df6b8b35e1136416cb61fd32bf762d2e
Frank Gerberding <frank.gerberding@smartsteuer.de>
2023-12-08 09:27:52 +0100

    day 08

---
+++ src/main/kotlin/de/smartsteuer/frank/adventofcode2023/day08/HauntedWasteland.kt
@@ -0,0 +1,65 @@
+package de.smartsteuer.frank.adventofcode2023.day08
+
+import de.smartsteuer.frank.adventofcode2023.lines
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.time.measureTime
+
+fun main() {
+  val map = parseMap(lines("/adventofcode2023/day08/map.txt"))
+  measureTime { println("part 1: ${part1(map)}") }.also { println("part 1 took $it") }
+  measureTime { println("part 2: ${part2(map)}") }.also { println("part 2 took $it") }
+}
+
+internal fun part1(desertMap: DesertMap): Long =
+  desertMap.followDirections("AAA") { it == "ZZZ" }
+
+internal fun part2(desertMap: DesertMap): Long =
+  desertMap.branches.keys.filter { it.endsWith('A') }.let { startPositions ->
+    lcm(startPositions.map { startPosition -> desertMap.followDirections(startPosition) { it.endsWith('Z') } })
+  }
+
+internal data class DesertMap(val directions: String, val branches: Map<String, Pair<String, String>>) {
+  fun followDirections(start: String, isTarget: (String) -> Boolean): Long {
+    tailrec fun followDirections(currentPosition: String, count: Long): Long {
+      if (isTarget(currentPosition)) return count
+      val direction   = directions[(count % directions.length).toInt()]
+      val branch      = branches[currentPosition] ?: throw IllegalStateException("invalid state: position = $currentPosition => no branch found")
+      val newPosition = if (direction == 'L') branch.first else branch.second
+      return followDirections(newPosition, count + 1)
+    }
+    return followDirections(start, 0)
+  }
+}
+
+internal fun lcm(values: List<Long>): Long =
+  values.drop(1).fold(values.first()) { acc, value -> max(acc, lcm(acc, value)) }
+
+internal fun lcm(number1: Long, number2: Long): Long {
+  if (number1 == 0L || number2 == 0L) return 0
+  val gcd = gcd(number1, number2)
+  return abs(number1 * number2) / gcd
+}
+
+internal tailrec fun gcd(number1: Long, number2: Long): Long {
+  if (number1 == 0L || number2 == 0L) return number1 + number2
+  val absNumber1 = abs(number1)
+  val absNumber2 = abs(number2)
+  val biggerValue = max(absNumber1, absNumber2)
+  val smallerValue = min(absNumber1, absNumber2)
+  return gcd(biggerValue % smallerValue, smallerValue)
+}
+
+internal fun parseMap(lines: List<String>): DesertMap {
+  val directions = lines.first()
+  val regex = """(\w{3}) = \((\w{3}), (\w{3})\)""".toRegex()
+  val branches = buildMap {
+    lines.drop(2).map { line ->
+      val matchResult = regex.matchEntire(line) ?: throw IllegalArgumentException("could not parse line $line")
+      val (_, from, left, right) = matchResult.groupValues
+      this[from] = left to right
+    }
+  }
+  return DesertMap(directions, branches)
+}
---
+++ src/test/kotlin/de/smartsteuer/frank/adventofcode2023/day08/HauntedWastelandTest.kt
@@ -0,0 +1,83 @@
+package de.smartsteuer.frank.adventofcode2023.day08
+
+import io.kotest.matchers.*
+import org.junit.jupiter.api.Test
+
+class HauntedWastelandTest {
+  private val input1 = listOf(
+    "RL",
+    "",
+    "AAA = (BBB, CCC)",
+    "BBB = (DDD, EEE)",
+    "CCC = (ZZZ, GGG)",
+    "DDD = (DDD, DDD)",
+    "EEE = (EEE, EEE)",
+    "GGG = (GGG, GGG)",
+    "ZZZ = (ZZZ, ZZZ)"
+  )
+
+  private val input2 = listOf(
+    "LLR",
+    "",
+    "AAA = (BBB, BBB)",
+    "BBB = (AAA, ZZZ)",
+    "ZZZ = (ZZZ, ZZZ)",
+  )
+
+  private val input3 = listOf(
+    "LR",
+    "",
+    "11A = (11B, XXX)",
+    "11B = (XXX, 11Z)",
+    "11Z = (11B, XXX)",
+  )
+
+  private val input4 = listOf(
+    "LR",
+    "",
+    "11A = (11B, XXX)",
+    "11B = (XXX, 11Z)",
+    "11Z = (11B, XXX)",
+    "22A = (22B, XXX)",
+    "22B = (22C, 22C)",
+    "22C = (22Z, 22Z)",
+    "22Z = (22B, 22B)",
+    "XXX = (XXX, XXX)",
+  )
+
+  @Test
+  fun `part 1`() {
+    part1(parseMap(input1)) shouldBe 2
+    part1(parseMap(input2)) shouldBe 6
+  }
+
+  @Test
+  fun `part 2`() {
+    part2(parseMap(input4)) shouldBe 6
+  }
+
+  @Test
+  fun `map can be parsed`() {
+    parseMap(input2) shouldBe DesertMap("LLR", mapOf(
+      "AAA" to ("BBB" to "BBB"),
+      "BBB" to ("AAA" to "ZZZ"),
+      "ZZZ" to ("ZZZ" to "ZZZ"),
+    ))
+    parseMap(input3) shouldBe DesertMap("LR", mapOf(
+      "11A" to ("11B" to "XXX"),
+      "11B" to ("XXX" to "11Z"),
+      "11Z" to ("11B" to "XXX"),
+    ))
+  }
+
+  @Test
+  fun lcm() {
+    lcm(12, 18) shouldBe 36
+  }
+
+  @Test
+  fun `lcm with multiple values`() {
+    lcm(listOf(12, 18)) shouldBe 36
+    lcm(listOf(3, 4, 5)) shouldBe 60
+  }
+}
