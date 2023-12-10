c1e4720ef1cb4a8216af62aacd9fb710d310dd1a
Frank Gerberding <frank.gerberding@smartsteuer.de>
2023-12-10 17:32:24 +0100

    shorter code, less understandable

--- src/main/kotlin/de/smartsteuer/frank/adventofcode2023/day09/MirageMaintenance.kt
+++ src/main/kotlin/de/smartsteuer/frank/adventofcode2023/day09/MirageMaintenance.kt
@@ -9,32 +9,19 @@ fun main() {
   measureTime { println("part 2: ${part2(histories)}") }.also { println("part 2 took $it") }
 }
 
-internal fun part1(histories: List<History>): Long =
-  histories.sumOf { it.computeNextNumber().toLong() }
+internal fun part1(histories: List<History>): Long = histories.sumOf { it.computeNextNumber().toLong() }
 
-internal fun part2(histories: List<History>): Long =
-  histories.sumOf { it.computePreviousNumber().toLong() }
+internal fun part2(histories: List<History>): Long = histories.sumOf { it.copy(numbers = it.numbers.reversed()).computeNextNumber().toLong() }
 
 internal data class History(val numbers: List<Int>) {
   private tailrec fun computeDifferences(result: List<List<Int>>): List<List<Int>> = when {
     result.last().all { it == 0 } -> result
-    else                          -> computeDifferences(result + listOf (result.last().computeDifferences()))
+    else                          -> computeDifferences(result + listOf(result.last().computeDifferences()))
   }
 
-  fun computeNextNumber(): Int =
-    computeDifferences(listOf(numbers)).let { differences ->
-      differences.reversed().drop(1).fold(0) { acc, list -> acc + list.last() }
-    }
-
-  fun computePreviousNumber(): Int =
-    computeDifferences(listOf(numbers)).let { differences ->
-      differences.reversed().drop(1).fold(0) { acc, list -> list.first() - acc }
-    }
+  fun computeNextNumber(): Int = computeDifferences(listOf(numbers)).sumOf { it.last() }
 }
 
 internal fun List<Int>.computeDifferences(): List<Int> = this.zipWithNext().map { (first, second) -> second - first }
 
-internal fun parseHistories(lines: List<String>): List<History> =
-  lines.map { line ->
-    History(line.split(" ").map { it.toInt() })
-  }
+internal fun parseHistories(lines: List<String>): List<History> = lines.map { line -> History(line.split(" ").map { it.toInt() }) }
