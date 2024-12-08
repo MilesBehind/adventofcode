3b22158483b7c1378a9a15072f50e9c6067175f2
Frank Gerberding <frank.gerberding@smartsteuer.de>
2024-12-08 09:43:21 +0100

    day 07

---
+++ src/main/kotlin/de/smartsteuer/frank/adventofcode2024/day08/ResonantCollinearity.kt
@@ -0,0 +1,60 @@
+package de.smartsteuer.frank.adventofcode2024.day08
+
+import de.smartsteuer.frank.adventofcode2024.Day
+import de.smartsteuer.frank.adventofcode2024.lines
+
+fun main() {
+  ResonantCollinearity.execute(lines("/adventofcode2024/day08/antenna-map.txt"))
+}
+
+object ResonantCollinearity: Day {
+
+  override fun part1(input: List<String>): Long =
+    input.parseMap().findAllAntiNodes().size.toLong()
+
+  override fun part2(input: List<String>): Long =
+    input.parseMap().findAllAntiNodesInLine().size.toLong()
+
+  data class Pos(val x: Int, val y: Int) {
+    operator fun plus(other: Pos):  Pos = Pos(x + other.x, y + other.y)
+    operator fun minus(other: Pos): Pos = Pos(x - other.x, y - other.y)
+    infix fun antiNodes(other: Pos): Pair<Pos, Pos> = (this - other).let { gap -> (this + gap) to (other - gap) }
+  }
+
+  data class AntennaMap(val antennas: Map<Pos, Char>, val width: Int, val height: Int) {
+    operator fun contains(pos: Pos) = pos.x in 0 until width && pos.y in 0 until height
+
+    fun findAntennasOfSameFrequencies(): List<List<Pos>> =
+      antennas.keys.groupBy { pos -> antennas.values.first { antennas[pos] == it } }.values.toList()
+
+    fun antiNodesInLine(antenna1: Pos, antenna2: Pos): Set<Pos> =
+      (antenna1 - antenna2).let { gap ->
+        generateSequence(antenna2) { pos -> (pos + gap).let { newPos -> if (newPos in this) newPos else null } }.toSet() +
+        generateSequence(antenna1) { pos -> (pos - gap).let { newPos -> if (newPos in this) newPos else null } }.toSet()
+      }
+
+    fun findAllAntiNodes(): Set<Pos> =
+      findAntennasOfSameFrequencies().flatMap { antennas: List<Pos> ->
+        antennas.pickTwo().flatMap { (antenna1, antenna2) ->
+          (antenna1 antiNodes antenna2).toList().filter { antenna -> antenna in this }
+        }
+      }.toSet()
+
+    fun findAllAntiNodesInLine(): Set<Pos> =
+      findAntennasOfSameFrequencies().flatMap { antennas: List<Pos> ->
+        antennas.pickTwo().flatMap { (antenna1, antenna2) ->
+          antiNodesInLine(antenna1, antenna2).toList().filter { antenna -> antenna in this }
+        }
+      }.toSet()
+  }
+
+  fun <T> List<T>.pickTwo(): List<Pair<T, T>> =
+    this.flatMapIndexed { index, value -> this.indices.drop(index + 1).map { value to this[it] } }
+
+  fun List<String>.parseMap(): AntennaMap =
+    AntennaMap(flatMapIndexed { y, line ->
+      line.mapIndexed { x, c ->
+        Pos(x, y) to c
+      }
+    }.filter { it.second != '.'}.toMap(), first().length, size)
+}
---
+++ src/test/kotlin/de/smartsteuer/frank/adventofcode2024/day08/ResonantCollinearityTest.kt
@@ -0,0 +1,69 @@
+package de.smartsteuer.frank.adventofcode2024.day08
+
+import de.smartsteuer.frank.adventofcode2024.day08.ResonantCollinearity.Pos
+import de.smartsteuer.frank.adventofcode2024.day08.ResonantCollinearity.parseMap
+import de.smartsteuer.frank.adventofcode2024.day08.ResonantCollinearity.part1
+import de.smartsteuer.frank.adventofcode2024.day08.ResonantCollinearity.part2
+import de.smartsteuer.frank.adventofcode2024.day08.ResonantCollinearity.pickTwo
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class ResonantCollinearityTest {
+  private val input = listOf(
+    "............",
+    "........0...",
+    ".....0......",
+    ".......0....",
+    "....0.......",
+    "......A.....",
+    "............",
+    "............",
+    "........A...",
+    ".........A..",
+    "............",
+    "............",
+  )
+
+  @Test
+  fun `part 1 returns expected result`() {
+    part1(input) shouldBe 14
+  }
+
+  @Test
+  fun `part 2 returns expected result`() {
+    part2(input) shouldBe 34
+  }
+
+  @Test
+  fun `antiNodes are computed as expected`() {
+    (Pos(4, 3) antiNodes Pos(5, 5)) shouldBe (Pos(3, 1) to Pos(6, 7))
+    (Pos(5, 5) antiNodes Pos(4, 3)) shouldBe (Pos(6, 7) to Pos(3, 1))
+  }
+
+  @Test
+  fun `antiNodes in line are computed as expected`() {
+    val map = input.parseMap()
+    map.antiNodesInLine(Pos(0, 0), Pos(1, 2)) shouldBe setOf(Pos(0, 0), Pos(1, 2), Pos(2, 4), Pos(3, 6), Pos(4, 8), Pos(5, 10))
+    map.antiNodesInLine(Pos(1, 2), Pos(0, 0)) shouldBe setOf(Pos(0, 0), Pos(1, 2), Pos(2, 4), Pos(3, 6), Pos(4, 8), Pos(5, 10))
+
+    map.antiNodesInLine(Pos(0, 0), Pos(3, 1)) shouldBe setOf(Pos(0, 0), Pos(3, 1), Pos(6, 2), Pos(9, 3))
+    map.antiNodesInLine(Pos(3, 1), Pos(0, 0)) shouldBe setOf(Pos(0, 0), Pos(3, 1), Pos(6, 2), Pos(9, 3))
+  }
+
+  @Test
+  fun `antennas with same  frequency can be found`() {
+    input.parseMap().findAntennasOfSameFrequencies() shouldBe listOf(
+      listOf(Pos(8, 1), Pos(5, 2), Pos(7, 3), Pos(4, 4)),
+      listOf(Pos(6, 5), Pos(8, 8), Pos(9, 9))
+    )
+  }
+
+  @Test
+  fun `all combinations of 2 elements can be picked from a list`() {
+    emptyList<Any>().pickTwo() shouldBe emptyList<Any>()
+    listOf(1).pickTwo() shouldBe emptyList<Any>()
+    listOf(1, 2).pickTwo() shouldBe listOf(1 to 2)
+    listOf(1, 2, 3).pickTwo() shouldBe listOf(1 to 2, 1 to 3, 2 to 3)
+    listOf(1, 2, 3, 4).pickTwo() shouldBe listOf(1 to 2, 1 to 3, 1 to 4, 2 to 3, 2 to 4, 3 to 4)
+  }
+}
